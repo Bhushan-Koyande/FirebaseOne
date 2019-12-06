7086f650c6609f55876e1612488eaee395acdbed
Bhushan-Koyande <53961964+Bhushan-Koyande@users.noreply.github.com>
2019-12-06 18:51:23 +0530

    bottom navigation done

--- app/src/main/java/com/example/firebaseone/MainActivity.java
+++ app/src/main/java/com/example/firebaseone/MainActivity.java
@@ -2,98 +2,84 @@ package com.example.firebaseone;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    private EditText mEmail,mPassword;
-    private Button signInBtn,signOutBtn;
-    private TextView signUpTextView,forgotTextView;
+
     private static final String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mEmail=findViewById(R.id.inputMail);
-        mPassword=findViewById(R.id.inputPassword);
-        signInBtn=findViewById(R.id.signInButton);
-        signOutBtn=findViewById(R.id.signOutButton);
-        signUpTextView=findViewById(R.id.signUpTextView);
-        forgotTextView=findViewById(R.id.forgotPasswdTextView);
-        mAuth=FirebaseAuth.getInstance();
-        mAuthListener=new FirebaseAuth.AuthStateListener(){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment());
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
-                FirebaseUser user=firebaseAuth.getCurrentUser();
-                if (user!=null){
-                    Log.d(TAG,"onAuthStateChanged:signed_in"+user.getUid());
-                    ToastMessage("Successfully signed in with :"+user.getEmail());
-                }else {
-                    Log.d(TAG,"onAuthStateChanged:signed_out");
-                    ToastMessage("Successfully signed out");
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment=null;
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                switch (menuItem.getItemId()){
+                    case R.id.nav_home:
+                        selectedFragment=new HomeFragment();
+                        break;
+                    case R.id.nav_search:
+                        selectedFragment=new SearchFragment();
+                        break;
+                    case R.id.nav_account:
+                        selectedFragment=new AccountFragment();
+                        break;
                 }
-            }
-        };
-        signInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email=mEmail.getText().toString();
-                String passwd=mPassword.getText().toString();
-                if (!email.equals("") && !passwd.equals("")){
-                    mAuth.signInWithEmailAndPassword(email,passwd);
-                }
-                else {
-                    ToastMessage("You didn't fill in all the required data");
-                }
+                fragmentManager.popBackStack("ConnectionError", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                assert selectedFragment!=null;
+                fragmentManager.beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                return true;
             }
         });
-        signOutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mAuth.signOut();
-                ToastMessage("Signing out");
-            }
-        });
-        signUpTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment());
 
-    private void ToastMessage(String s) {
-        Toast.makeText(this,s,Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        //FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser);
-        mAuth.addAuthStateListener(mAuthListener);
     }
     @Override
-    public void onStop(){
-        super.onStop();
-        if (mAuthListener!=null){
-            mAuth.removeAuthStateListener(mAuthListener);
+    public void onBackPressed(){
+        Intent intent=new Intent(MainActivity.this,SignInActivity.class);
+        startActivity(intent);
+    }
+
+    //@Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
+        Fragment selectedFragment=null;
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                selectedFragment=new HomeFragment();
+                break;
+            case R.id.nav_search:
+                selectedFragment=new SearchFragment();
+                break;
+            case R.id.nav_account:
+                selectedFragment=new AccountFragment();
+                break;
         }
+        fragmentManager.popBackStack("ConnectionError", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        assert selectedFragment!=null;
+        fragmentManager.beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+        return true;
     }
 
 }
