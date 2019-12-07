8d62499d5a2c59d11adccf34f36e5698f8449aec
Bhushan-Koyande <53961964+Bhushan-Koyande@users.noreply.github.com>
2019-12-07 13:28:12 +0530

    bottom navigation done

---
+++ app/src/main/java/com/example/firebaseone/SignInActivity.java
@@ -0,0 +1,107 @@
+package com.example.firebaseone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignInActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText mEmail,mPassword;
+    private Button signInBtn,signOutBtn;
+    private TextView signUpTextView,forgotTextView;
+    private static final String TAG="MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.avtivity_signin);
+        mEmail=findViewById(R.id.inputMail);
+        mPassword=findViewById(R.id.inputPassword);
+        signInBtn=findViewById(R.id.signInButton);
+        signOutBtn=findViewById(R.id.signOutButton);
+        signUpTextView=findViewById(R.id.signUpTextView);
+        forgotTextView=findViewById(R.id.forgotPasswdTextView);
+        mAuth=FirebaseAuth.getInstance();
+        mAuthListener=new FirebaseAuth.AuthStateListener(){
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+                FirebaseUser user=firebaseAuth.getCurrentUser();
+                if (user!=null){
+                    Log.d(TAG,"onAuthStateChanged:signed_in"+user.getUid());
+                    ToastMessage("Successfully signed in with :"+user.getEmail());
+                }else {
+                    Log.d(TAG,"onAuthStateChanged:signed_out");
+                    ToastMessage("Successfully signed out");
+                }
+            }
+        };
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=mEmail.getText().toString();
+                String passwd=mPassword.getText().toString();
+                if (!email.equals("") && !passwd.equals("")){
+                    mAuth.signInWithEmailAndPassword(email,passwd).addOnCompleteListener(SignInActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Intent i=new Intent(SignInActivity.this,MainActivity.class);
+                            startActivity(i);
+                        }
+                    });
+
+                }
+                else {
+                    ToastMessage("You didn't fill in all the required data");
+                }
+            }
+        });
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                ToastMessage("Signing out");
+            }
+        });
+        signUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(SignInActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void ToastMessage(String s) {
+        Toast.makeText(this,s,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        //FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        if (mAuthListener!=null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+}
